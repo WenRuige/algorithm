5f4bc8284325af3b762e809dc907ea701537ff8c
gewenrui <941137860@qq.com>
2018-08-22 17:00:37 +0800

    add some function

--- matrixReshape.go
+++ matrixReshape.go
@@ -7,7 +7,33 @@ func matrixReshape(nums [][]int, r int, c int) [][]int {
 	if len(nums)*len(nums[0]) < r*c {
 		return nums
 	}
+	// 分配一个数组来存储
+	// golang 声明一个二维数组
 
+	newArr := []int{}
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums[i]); j++ {
+			newArr = append(newArr, nums[i][j])
+		}
+	}
+
+	finalArr := [][]int{}
+	tempArr := []int{}
+	temp := 0
+	for i := 0; i < len(newArr); i++ {
+
+		if temp == c-1 {
+			tempArr = append(tempArr, newArr[i])
+			finalArr = append(finalArr, tempArr)
+			tempArr = []int{}
+			temp = 0
+		} else {
+			tempArr = append(tempArr, newArr[i])
+			temp++
+		}
+	}
+
+	return finalArr
 }
 
 func main() {
