cbef867431a303102ef7cff415901067bf4d461f
gewenrui <941137860@qq.com>
2018-08-23 11:44:46 +0800

    add some algorithm

--- isToeplitzMatrix.go
+++ isToeplitzMatrix.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+func isToeplitzMatrix(matrix [][]int) bool {
+
+	for i := 0; i < len(matrix)-1; i++ {
+		for j := 0; j < len(matrix[i])-1; j++ {
+			if matrix[i][j] != matrix[i+1][j+1] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func main() {
+
+	nums := [][]int{{1, 2, 3, 4}, {5, 1, 2, 3}, {9, 5, 1, 2}}
+	res := isToeplitzMatrix(nums)
+	fmt.Println(res)
+}
---
+++ permute.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+var ret [][]int
+
+func help(nums []int, idx int) {
+	if idx >= len(nums) {
+		return
+	}
+	help(nums, idx+1)
+	for i := 0; i < idx; i++ {
+		tmp := nums[i]
+		nums[i] = nums[idx]
+		nums[idx] = tmp
+		tmp1 := make([]int, len(nums))
+		copy(tmp1, nums)
+		ret = append(ret, tmp1)
+		help(nums, idx+1)
+		tmp = nums[i]
+		nums[i] = nums[idx]
+		nums[idx] = tmp
+	}
+}
+func permute(nums []int) [][]int {
+	ret = [][]int{}
+	tmp := make([]int, len(nums))
+	copy(tmp, nums)
+	ret = append(ret, tmp)
+	help(nums, 1)
+	return ret
+}
+
+func main() {
+	nums := []int{1, 2, 3}
+	res := permute(nums)
+	fmt.Println(res)
+}
