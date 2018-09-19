300950ea8bf2c9a6eb6cad04573e39931ed67fe3
gewenrui <941137860@qq.com>
2018-09-19 17:08:45 +0800

    update

---
+++ rotatedDigits.go
@@ -0,0 +1,40 @@
+package main
+
+func rotatedDigits(N int) int {
+	count := 0
+	for i := 1; i <= N; i++ {
+		if isValids(i) {
+			count++
+		}
+	}
+	return count
+}
+
+func isValids(n int) bool {
+	ans := false
+	for 0 < n {
+		if 2 == n%10 {
+			ans = true
+		}
+		if 5 == n%10 {
+			ans = true
+		}
+		if 6 == n%10 {
+			ans = true
+		}
+		if 9 == n%10 {
+			ans = true
+		}
+		if 3 == n%10 {
+			return false
+		}
+		if 4 == n%10 {
+			return false
+		}
+		if 7 == n%10 {
+			return false
+		}
+		n /= 10
+	}
+	return ans
+}
