780f4a4cc50767aaab1079f55e42bc9d22cdefca
gewenrui <gewenrui@didichuxing.com>
2018-10-05 12:58:18 +0800

    add all function

---
+++ repeatedSubstringPattern.go
@@ -0,0 +1,14 @@
+package main
+
+
+
+
+
+func repeatedSubstringPattern(s string) bool {
+
+}
+
+
+func main(){
+
+}
---
+++ roeate.go
@@ -0,0 +1,14 @@
+package main
+
+
+
+func rotate(nums []int, k int)  {
+
+}
+
+
+
+func main(){
+	nums:=[]int{1,2,3,4,5,6,7,8,9,10}
+	rotate(nums,1)
+}
