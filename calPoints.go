2a56e249601768d8d5c43935789475c630c06c0e
gewenrui <941137860@qq.com>
2018-10-09 18:43:05 +0800

    add baseball match

--- calPoints.go
+++ calPoints.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func calPoints(ops []string) int {
+	newArr := []int{}
+	for i := 0; i < len(ops); i++ {
+		// 表示最后一个回合是无效的,应该被移除
+		if ops[i] == "C" {
+			newArr = newArr[0 : len(newArr)-1]
+			// 表示本轮获得的得分是前一轮有效 回合得分的两倍
+		} else if ops[i] == "D" {
+			newArr = append(newArr, newArr[len(newArr)-1]*2)
+			//表示本轮获得的得分是前两轮有效 回合得分的总和
+		} else if ops[i] == "+" {
+			res := newArr[len(newArr)-1] + newArr[len(newArr)-2]
+			newArr = append(newArr, res)
+		} else {
+			newNum, _ := strconv.Atoi(ops[i])
+			newArr = append(newArr, newNum)
+		}
+	}
+	sum := 0
+	for _, v := range newArr {
+		sum = sum + v
+	}
+	return sum
 
 }
 
