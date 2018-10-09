13c29084e1e6eabde88cf95dbca97f88fbd8baa6
gewenrui <941137860@qq.com>
2018-10-09 19:10:50 +0800

    add原地算法

--- removeElement.go
+++ removeElement.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func removeElement(nums []int, val int) int {
+	// 快慢指针
+	fast, slow := 0, 0
+
+	for fast < len(nums) {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	nums = nums[0:slow]
+	return slow
 }
 
 func main() {
