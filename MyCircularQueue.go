e3e2e4a60bae5ba31cd567de48f46eae95f66b57
941137860@qq.com <941137860@qq.com>
2018-10-12 07:24:48 +0800

    add mycircularqueue

---
+++ MyCircularQueue.go
@@ -0,0 +1,60 @@
+package main
+
+
+type MyCircularQueue struct {
+
+}
+
+
+/** Initialize your data structure here. Set the size of the queue to be k. */
+func ConstructorQi(k int) MyCircularQueue {
+
+}
+
+
+/** Insert an element into the circular queue. Return true if the operation is successful. */
+func (this *MyCircularQueue) EnQueue(value int) bool {
+
+}
+
+
+/** Delete an element from the circular queue. Return true if the operation is successful. */
+func (this *MyCircularQueue) DeQueue() bool {
+
+}
+
+
+/** Get the front item from the queue. */
+func (this *MyCircularQueue) Front() int {
+
+}
+
+
+/** Get the last item from the queue. */
+func (this *MyCircularQueue) Rear() int {
+
+}
+
+
+/** Checks whether the circular queue is empty or not. */
+func (this *MyCircularQueue) IsEmpty() bool {
+
+}
+
+
+/** Checks whether the circular queue is full or not. */
+func (this *MyCircularQueue) IsFull() bool {
+
+}
+
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * obj := Constructor(k);
+ * param_1 := obj.EnQueue(value);
+ * param_2 := obj.DeQueue();
+ * param_3 := obj.Front();
+ * param_4 := obj.Rear();
+ * param_5 := obj.IsEmpty();
+ * param_6 := obj.IsFull();
+ */
--- repeatedSubstringPattern.go
+++ repeatedSubstringPattern.go
@@ -1,14 +1,31 @@
 package main
 
-
-
-
+import "fmt"
 
 func repeatedSubstringPattern(s string) bool {
-
+	if len(s) <= 1 {
+		return false
+	}
+	for i := len(s) / 2; i > 0; i-- {
+		// 循环的次数,必须是一个可除开的数
+		if len(s)%i == 0 {
+			// 子串的数量
+			substr := len(s) / i
+			// 获取前i个数,看拼起来的是否和本身相同
+			sub := s[0:i]
+			temp := ""
+			for j := 0; j < substr; j++ {
+				temp = temp + sub
+			}
+			if temp == s {
+				return true
+			}
+		}
+	}
+	return false
 }
 
-
-func main(){
-
+func main() {
+	res := repeatedSubstringPattern("gwrgwrgwrs")
+	fmt.Println(res)
 }
