480755c5ca0608d663c00af22a4bb991fc46b116
941137860@qq.com <941137860@qq.com>
2018-10-23 07:39:53 +0800

    ADD

---
+++ numberOfBoomerangs.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func numberOfBoomerangs(points [][]int) int {
+	res := 0
+
+	size := len(points)
+	if size < 3 {
+		return 0
+	}
+
+	for i := 0; i < size; i++ {
+		// dMap[9]6 表示， points 中与 points[i] 距离为 3 的点，共有 6 个。
+		dMap := make(map[int]int, size)
+
+		for j := 0; j < size; j++ {
+			if i == j {
+				continue
+			}
+
+			d := dSquare(points[i], points[j])
+
+			if _, ok := dMap[d]; ok {
+				dMap[d]++
+			} else {
+				dMap[d] = 1
+			}
+		}
+		fmt.Println(dMap)
+
+		for _, v := range dMap {
+			// 相同距离的 v 个点
+			// 总共有 v * (v - 1) 种排列
+			res += v * (v - 1)
+		}
+	}
+
+	return res
+}
+
+func dSquare(p1, p2 []int) int {
+	x := p2[0] - p1[0]
+	y := p2[1] - p1[1]
+	return x*x + y*y
+}
+
+func main(){
+	fmt.Println(numberOfBoomerangs([][]int{{0,0},{1,0},{2,0}}))
+}
