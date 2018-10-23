990f00106b2620a573a4d1f29570961019de9aff
gewenrui <941137860@qq.com>
2018-10-23 20:59:09 +0800

    convertBST

---
+++ convertBST.go
@@ -0,0 +1,18 @@
+package main
+
+func convertBST(root *TreeNode) *TreeNode {
+	sum := 0
+	travel(root, &sum)
+	return root
+}
+
+// 从大到小遍历 BST 并沿路更新 sum 和 节点值
+func travel(root *TreeNode, sum *int) {
+	if root == nil {
+		return
+	}
+	travel(root.Right, sum)
+	*sum += root.Val
+	root.Val = *sum
+	travel(root.Left, sum)
+}
