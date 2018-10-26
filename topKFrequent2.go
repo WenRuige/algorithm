3a8ae35ab55adbc8cc6b3cc350d89d356d15ce4f
gewenrui <941137860@qq.com>
2018-10-26 10:26:36 +0800

    update

--- topKFrequent2.go
+++ topKFrequent2.go
@@ -1,29 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"sort"
+)
 
-// 692
-func topKFrequent2(words []string, k int) []string {
+type strStruct struct {
+	word      string
+	frequence int
+}
+type freWords []*strStruct
 
-	str := make(map[string]int)
-	for i := 0; i < len(words); i++ {
-		_, ok := str[words[i]]
-		if ok {
-			str[words[i]] = str[words[i]] + 1
-		} else {
-			str[words[i]] = 1
-		}
-	}
+func (f freWords) Len() int {
+	return len(f)
 
-	// 找出前两个元素
-
-	fmt.Println(str)
-
-
-
-	return []string{}
 }
 
-func main(){
-	topKFrequent2([]string{"i", "love", "leetcode", "i", "love", "coding"},2)
+// 进行交换
+func (f freWords) Swap(i, j int) {
+	f[i], f[j] = f[j], f[i]
+}
+func (f freWords) Less(i, j int) bool {
+	// 如果连个词出现的次数相同的时候,需要判断word的长度
+	if f[i].frequence == f[j].frequence {
+		return f[i].word < f[j].word
+	}
+	return f[i].frequence > f[j].frequence
+}
+
+func topKFrequent2(words []string, k int) []string {
+	count := make(map[string]int, len(words))
+
+	for _, w := range words {
+		count[w]++
+	}
+
+	// 将数据都解析到struct上
+	fw := make(freWords, 0, len(words))
+	for w, c := range count {
+		fw = append(fw, &strStruct{
+			word:      w,
+			frequence: c,
+		})
+	}
+	sort.Sort(fw)
+
+	res := make([]string, k)
+	for i := 0; i < k; i++ {
+		res[i] = fw[i].word
+	}
+
+	return res
+}
+
+func main() {
+	topKFrequent2([]string{"i", "love", "leetcode", "i", "love", "coding"}, 2)
 }
