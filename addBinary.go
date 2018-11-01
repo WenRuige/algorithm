4d2a96775d2d1f73b07989c7061c870803e21001
gewenrui <941137860@qq.com>
2018-11-01 18:25:18 +0800

    add longestPalindrome

--- addBinary.go
+++ addBinary.go
@@ -2,53 +2,48 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
-
-//func addBinary(a string, b string) string {
-//
-//	if a == "0" && b == "0" {
-//		return "0"
-//	}
-//	aSum := 0
-//	bSum := 0
-//	k := 0
-//	for i := len(a) - 1; i >= 0; i-- {
-//		intNum, _ := strconv.Atoi(string(a[i]))
-//		aSum = aSum + int(math.Pow(2, float64(k)))*intNum
-//		k++
-//	}
-//	k = 0
-//	for i := len(b) - 1; i >= 0; i-- {
-//		intNum, _ := strconv.Atoi(string(b[i]))
-//		bSum = bSum + int(math.Pow(2, float64(k)))*intNum
-//		k++
-//	}
-//
-//	total := aSum + bSum
-//
-//	fmt.Println(total)
-//	data := []int{}
-//	for total > 0 {
-//		pop := total % 2
-//		total = total / 2
-//		data = append(data, pop)
-//	}
-//
-//	data2 := ""
-//
-//	for i := len(data) - 1; i >= 0; i-- {
-//		data2 = data2 + strconv.Itoa(data[i])
-//	}
-//
-//	return data2
-//}
 
 func addBinary(a string, b string) string {
 
+	if len(a) > len(b) {
+		res := len(a) - len(b)
+		for i := 0; i < res; i++ {
+			b = "0" + b
+		}
+	}
+	if len(a) < len(b) {
+		res := len(b) - len(a)
+		for i := 0; i < res; i++ {
+			a = "0" + a
+		}
+	}
+	// 前置补0
+	str := ""
+	flag := 0
+	for i := len(a) - 1; i >= 0; i++ {
+		int1, _ := strconv.Atoi(string(a[i]))
+		int2, _ := strconv.Atoi(string(b[i]))
+		if int1+int2 > 2 {
+			// 二进制与操作进0
+			flag = 1
+			str = str + "0"
+		} else {
+			if flag == 1 {
+				str = str + "0"
+			} else {
+				str = str + "1"
+			}
+
+		}
+	}
+
+	fmt.Println(a, b)
 	return ""
 }
 
 func main() {
-	res := addBinary("1010", "1011")
+	res := addBinary("1011", "1011")
 	fmt.Println(res)
 }
--- longestPalindrome.go
+++ longestPalindrome.go
@@ -5,15 +5,37 @@ import "fmt"
 // 最长回文串
 
 func longestPalindrome(s string) int {
-
+	str := make(map[string]int)
 	for i := 0; i < len(s); i++ {
-		fmt.Println(string(s[i]))
+		_, ok := str[string(s[i])]
+		if ok {
+			str[string(s[i])] = str[string(s[i])] + 1
+		} else {
+			str[string(s[i])] = 1
+		}
+
 	}
-	return 0
+	final := 0
+	flag := 0
+	for _, v := range str {
+		//fmt.Println(k, v)
+		if v%2 == 0 {
+			final = final + v
+		} else {
+			final = final + v - 1
+			flag = 1
+		}
+	}
+
+	if flag == 1 {
+		return final + 1
+	}
+
+	return final
 
 }
 
 func main() {
-	res := longestPalindrome("abccccdd")
+	res := longestPalindrome("ccc")
 	fmt.Println(res)
 }
