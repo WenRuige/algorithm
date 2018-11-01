1fbdb1da7ded1add721a5fcf1ceccf5749ba90a4
gewenrui <941137860@qq.com>
2018-11-01 11:07:00 +0800

    add max area of island

--- maxAreaOfIsland.go
+++ maxAreaOfIsland.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func maxAreaOfIsland(grid [][]int) int {
+	//dfs 深度优先搜索
+	m := len(grid)
+	n := len(grid[0])
+	max := 0
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] != 0 {
+				max = int(math.Max(float64(max), float64(areaIsLand(grid, i, j))))
+			}
+
+		}
+	}
+
+	return max
+}
+
+func areaIsLand(grid [][]int, i, j int) int {
+	if i >= 0 && i < len(grid) && j >= 0 && j < len(grid[0]) && grid[i][j] != 0 {
+		grid[i][j] = 0
+		return 1 + areaIsLand(grid, i+1, j) + areaIsLand(grid, i-1, j) + areaIsLand(grid, i, j+1) + areaIsLand(grid, i, j-1)
+	}
+	return 0
 
 }
 
