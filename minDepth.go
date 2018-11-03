c64ce588afb718466c762697e7942906e1c55d7c
941137860@qq.com <941137860@qq.com>
2018-11-03 09:39:34 +0800

    add

---
+++ minDepth.go
@@ -0,0 +1,19 @@
+package main
+func minDepth(root *TreeNode) int {
+	switch {
+	case root == nil:
+		return 0
+	case root.Left == nil:
+		return 1 + minDepth(root.Right)
+	case root.Right == nil:
+		return 1 + minDepth(root.Left)
+	default:
+		return 1 + min(minDepth(root.Left), minDepth(root.Right))
+	}
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
