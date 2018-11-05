45c9e70b06f4a6f415540c82886a5c17ccbffe1d
gewenrui <941137860@qq.com>
2018-11-05 11:54:08 +0800

    add

--- constructRectangle.go
+++ constructRectangle.go
@@ -5,29 +5,50 @@ import (
 	"math"
 )
 
+//func constructRectangle(area int) []int {
+//	temp := math.Sqrt(float64(area))
+//	res := []int{}
+//	if temp*temp == float64(area) {
+//		return []int{int(temp), int(temp)}
+//	}
+//	temp = math.Floor(temp)
+//	fmt.Println(temp)
+//	second := area / int(temp)
+//
+//	if second > int(temp) {
+//		res = append(res, second)
+//		res = append(res, int(temp))
+//	} else {
+//		res = append(res, int(temp))
+//		res = append(res, second)
+//
+//	}
+//
+//	return res
+//}
+
 func constructRectangle(area int) []int {
 	temp := math.Sqrt(float64(area))
 	res := []int{}
-	if temp*temp == float64(area) {
+	if int(temp)*int(temp) == area {
 		return []int{int(temp), int(temp)}
 	}
 	temp = math.Floor(temp)
-	fmt.Println(temp)
-	second := area / int(temp)
 
-	if second > int(temp) {
-		res = append(res, second)
-		res = append(res, int(temp))
-	} else {
-		res = append(res, int(temp))
-		res = append(res, second)
+	for i := temp; i > 0; i-- {
+		fmt.Println(area % int(i))
+		if area%int(i) == 0 {
+
+			res = append(res, area/int(i))
+			res = append(res, int(i))
+			break
+		}
 
 	}
-
 	return res
 }
 
 func main() {
-	res := constructRectangle(5)
+	res := constructRectangle(2)
 	fmt.Println(res)
 }
